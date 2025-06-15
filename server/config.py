10aec147b2b5e92815086b6520f7cc1b3ef6332b
geekinryxnn <ryankiratu67@gmail.com>
2025-06-15 20:28:06 +0300

    new models codes

---
+++ server/config.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pizza_restaurant.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
